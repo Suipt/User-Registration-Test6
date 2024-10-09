208d331caa45b8d691bd25fc9b8fc1834d21d470
ismailbilal <is123bilal@gmail.com>
2024-10-09 18:52:15 +0100

    adding header

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -1,5 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./components/header.js/Header";
+
+// const router = createBrowserRouter([]);
+
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="flex justify-center w-full min-h-screen relative items-center items-start bg-sky-100">
+      <Header />
+      {/* <RouterProvider router={router} /> */}
+    </div>
+  );
 }
 
 export default App;
---
+++ front-end-react/src/components/header.js/Header.js
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <div className=" p-4 px-8 capitalize text-3xl font-bold z-10 fixed right-0 left-0 top-0 bg-white shadow-md">
+      auth application
+    </div>
+  );
+};
